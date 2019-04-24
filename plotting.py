b0fcec4aec46bed1a63aca67957fb86186f8e863
Markus Radziej <markus.radziej@gmail.com>
2019-04-24 21:37:52 +0200

    Tighten plot layout and add visualization of folds
    This commit introduces tight_layout to reduce whitespace of plots. It
    also adds a function to visualize stratified folds.

--- plotting.py
+++ plotting.py
@@ -1,6 +1,8 @@
 import os
 import itertools
 
+import numpy as np
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -9,8 +11,10 @@ def palette():
     return itertools.cycle(sns.color_palette())
 
 
-def save_figure(plot, name, suffix='.png', directory='plots'):
+def save_figure(plot, name, suffix='.pdf', directory='plots'):
     '''Save pyplot/seaborn plot to the directory with the given name'''
+
+    plt.tight_layout()
 
     if hasattr(plot, 'savefig'):
         plot.savefig(os.path.join(directory, name + suffix))
@@ -31,3 +35,34 @@ def confusion_heatmap(confusion_matrix, labels, normalize=False):
                      yticklabels=labels)
     plt.xlabel('Predicted Label')
     plt.ylabel('True Label')
+
+
+def cross_validation_indices(cv, X, y, ax, n_splits, lw=10):
+    '''Create a sample plot for indices of a cross-validation object.'''
+
+    # Generate the training/testing visualizations for each CV split
+    for ii, (tr, tt) in enumerate(cv.split(X=X, y=y)):
+        # Fill in indices with the training/test groups
+        indices = np.array([np.nan] * len(X))
+        indices[tt] = 1
+        indices[tr] = 0
+
+        # Visualize the results
+        ax.scatter(range(len(indices)), [ii + .5] * len(indices),
+                   c=indices, marker='_', lw=lw, cmap=plt.cm.coolwarm,
+                   vmin=-.2, vmax=1.2)
+
+    # Plot the data classes at the end
+    ax.scatter(range(len(X)), [ii + 1.5] * len(X),
+               c=y, marker='_', lw=lw, cmap=plt.cm.Paired)
+
+    # ax.scatter(range(len(X)), [ii + 2.5] * len(X),
+    #            c=group, marker='_', lw=lw, cmap=cmap_data)
+
+    # Formatting
+    yticklabels = list(range(n_splits)) + ['class']
+    ax.set(yticks=np.arange(n_splits+2) + .5, yticklabels=yticklabels,
+           xlabel='Sample index', ylabel="CV iteration",
+           ylim=[n_splits+1.2, -.2], xlim=[0, len(X)])
+    ax.set_title('{}'.format(type(cv).__name__), fontsize=15)
+    return ax
